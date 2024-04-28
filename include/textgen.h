2b2b93f2942d4f26a421e221c2141980b805c1c6
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2024-04-28 21:31:58 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,20 +1,20 @@
 //Copyright 2024 UNN-IASR
 #pragma once
-#include <vector>
-#include <deque>
-#include <sstream>
 #include <string>
+#include <sstream>
+#include <deque>
 #include <map>
+#include <vector>
 
 typedef std::deque<std::string> prefix;
 class TextGen {
  public:
     std::map<prefix, std::vector<std::string> > tab_state;
-    prefix f;
+    prefix first;
     void StateTable(
         const std::map<prefix,
         std::vector<std::string>>&);
     TextGen();
-    void create_tab(const std::string& text, int number);
-    std::string generate(int number, unsigned int time_num);
+    void create_tab(const std::string& text, int count);
+    std::string generate(int count, unsigned int time_count);
 };
